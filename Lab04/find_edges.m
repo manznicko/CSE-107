b2c52183f5884037dee2ebda002b7f089e643a71
manznicko <nicholas7299@gmail.com>
2022-04-20 15:54:32 -0700

    Finished lab

--- Lab04/find_edges.m
+++ Lab04/find_edges.m
@@ -1,10 +1,22 @@
 function [edges] = find_edges(inp, t)
 % FIND EDGES Summar of function goes here:
+[M,N] = size(inp);
+edges = zeros(M,N);
 
 g = gradient_magnitude(inp);
 
 % % threshold here
+% From pdf: value 255 where edges are detected and 0 elsewhere
 % edges = %
+for m=1:M
+    for n=1:N
+        if(g(m,n) > t)
+            edges(m,n) = 255;
+        else
+            edges(m,n) = 0;
+        end
+    end
+end
 
-
+edges = uint8(edges);
 end
--- Lab04/gradient_magnitude.m
+++ Lab04/gradient_magnitude.m
@@ -1,5 +1,7 @@
-function [img, grad] = gradient_magnitude(input)
+function [grad] = gradient_magnitude(input)
 % GRADIENT MAGNITUDE Summary of this function goes here:
+[M,N] = size(input);
+grad = zeros(M,N);
 
 sobel_x= [1 ,0 ,-1; 2,0,-2; 1, 0 ,-1];
 sobel_y= [1,2,1; 0,0, 0; -1, -2 ,-1];
@@ -7,7 +9,7 @@ sobel_y= [1,2,1; 0,0, 0; -1, -2 ,-1];
 grad_x = spatial_filter(input, sobel_x);
 grad_y = spatial_filter(input, sobel_y);
 
-grad = sqrt(grad_x.^2 + grad_y.^2);
+grad = double(sqrt(grad_x.^2 + grad_y.^2));
 
 
 end
